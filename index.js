c8a7ffa15f6616965dae536c2a2065096e23ad25
mariah622 <mjustine622@gmail.com>
2021-12-09 15:45:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+
+
+function changeText() {
+    let change = document.getElementById('text')
+    change.innerHTML = "This is really cool!"
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+   changeText()
+})
