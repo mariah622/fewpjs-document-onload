043a2b1cd694f25f848e03221fd8b351a33388a8
mariah622 <mjustine622@gmail.com>
2022-01-26 19:53:59 -0500

    Done.

--- index.js
+++ index.js
@@ -2,12 +2,25 @@
 
 
 
-function changeText() {
-    let change = document.getElementById('text')
-    change.innerHTML = "This is really cool!"
-}
+// function changeText() {
+//     let change = document.getElementById('text')
+//     change.innerHTML = "This is really cool!"
+// }
 
 
-document.addEventListener("DOMContentLoaded", function() {
-   changeText()
+// document.addEventListener("DOMContentLoaded", function() {
+//    changeText()
+// })
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    changeText();
+
 })
+
+function changeText(){
+    const text = document.getElementById('text');
+    text.innerHTML = "This is really cool!"
+}
